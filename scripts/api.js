945644a384491c3f254ed826a661e265852c2f75
jpp7557 <jp75.tran@gmail.com>
2024-10-22 10:48:03 +0200

    add file scripts/api.js

---
+++ scripts/api.js
@@ -0,0 +1,15 @@
+// Fetch data from the JSON file
+export async function getJsonArrays(url) {
+    try {
+        const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch json file from ', url);
+    }
+    const data = await response.json();
+    console.log("data :",data);
+    return data; // Return all arrays found from the JSON file
+    } catch (error) {
+        console.error('Error No Array fetched.', error);
+        return { photographers: [] }; // Return an empty array in case of error
+    }
+}
